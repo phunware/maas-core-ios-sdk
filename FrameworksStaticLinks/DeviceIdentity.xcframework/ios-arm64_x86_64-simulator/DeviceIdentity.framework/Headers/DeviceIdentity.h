bea8c5ebdfe2b918e5e1f771645c0c496feb6fab
Kent Tu <hostmaster@phunware.com>
2022-01-26 15:10:40 -0800

    Update version 3.12.0. See CHANGELOG for details.

---
+++ FrameworksStaticLinks/DeviceIdentity.xcframework/ios-arm64_x86_64-simulator/DeviceIdentity.framework/Headers/DeviceIdentity.h
@@ -0,0 +1,16 @@
+//
+//  DeviceIdentity.h
+//  PWCore
+//
+//  Created by Kent Tu on 4/21/21.
+//  Copyright © 2021 Phunware. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#ifndef DeviceIdentity_h
+#define DeviceIdentity_h
+
+static NSString * const DeviceIdentityVersion = @"3.12.0";
+
+#endif /* DeviceIdentity_h */
